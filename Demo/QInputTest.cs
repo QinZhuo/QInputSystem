410c096035f6b4232994725d32a3d7020e051f88
琴卓 <1043665802@qq.com>
2022-07-14 20:38:52 +0800

    test

--- Demo/QInputTest.cs
+++ Demo/QInputTest.cs
@@ -3,37 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using QTool.QInputSystem;
 using UnityEngine.InputSystem;
-public class QInputTest : MonoBehaviour
+namespace QTool
 {
-    public InputActionReference input;
-    // Start is called before the first frame update
-    void Start()
+
+    public class QInputTest : MonoBehaviour
     {
-        input.action.Enable();
-        input.action.started += (info) =>
+        public InputActionReference input;
+        // Start is called before the first frame update
+        void Start()
         {
-            QDebug.Log("start");
-        };
-        input.action.performed += (info) =>
+            input.action.Enable();
+            input.action.started += (info) =>
+            {
+                QDebug.Log("start");
+            };
+            input.action.performed += (info) =>
+            {
+                QDebug.Log("performed");
+            };
+            input.action.canceled += (info) =>
+            {
+                QDebug.Log("canceled");
+            };
+        }
+
+        // Update is called once per frame
+        void Update()
         {
-            QDebug.Log("performed");
-        };
-        input.action.canceled += (info) =>
-        {
-            QDebug.Log("canceled");
-        };
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.LogError(input.action.phase);
-        //获取移动摇杆输入方向
-        //var moveDir = QInput.Actions["移动"].Vector2 * 100 * Time.deltaTime;
-        //transform.localPosition +=new Vector3(moveDir.x, moveDir.y,0);
+            Debug.LogError(input.action.phase);
+            //获取移动摇杆输入方向
+            //var moveDir = QInput.Actions["移动"].Vector2 * 100 * Time.deltaTime;
+            //transform.localPosition +=new Vector3(moveDir.x, moveDir.y,0);
 
 
-        ////限制移动范围
-        //transform.localPosition = Vector3.ClampMagnitude(transform.localPosition, 300);
+            ////限制移动范围
+            //transform.localPosition = Vector3.ClampMagnitude(transform.localPosition, 300);
+        }
     }
 }
