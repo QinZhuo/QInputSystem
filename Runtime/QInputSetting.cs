e53d1c656e4bc352d8cfc0aac51ffbcf74b05ff8
琴卓 <1043665802@qq.com>
2022-10-12 18:06:39 +0800

    QInputSetting

--- Runtime/QInputSetting.cs
+++ Runtime/QInputSetting.cs
@@ -1,15 +1,88 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using QTool;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using System;
 
-namespace QTool.InputSystem
-{
+namespace QTool.InputSystem {
+
     using InputSystem = UnityEngine.InputSystem.InputSystem;
-    public class QInputSetting : QInputTip
+    /// <summary>
+    /// 快捷键提示脚本
+    /// </summary>
+    public class QInputSetting : MonoBehaviour
     {
+        public InputActionReference action;
+        [SerializeField]
+        private string tipInfo = "按下{Key}触发";
+        protected string ViewKey
+        {
+            get
+            {
+                return action.action?.GetBindingDisplayString();
+            }
+        }
+        public string TipInfo
+        {
+            get
+            {
+                return tipInfo;
+            }
+            set
+            {
+                tipInfo = value;
+                OnChange();
+            }
+        }
+        public StringEvent OnValueChange;
+        public bool Active
+        {
+            get
+            {
+                return action!=null&&action.action != null;
+            }
+        }
+
+        protected void OnChange()
+        {
+            if (!Active) return;
+            string tip = "";
+            tip = tipInfo.Replace("{Key}", ViewKey);
+            OnValueChange?.Invoke(tip);
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+           
+            OnChange();
+        }
+# endif
+        protected virtual void OnEnable()
+        {
+            OnChange();
+            OnChangeKey += OnChangeKeyCallBack;
+            QInputSystem.OnControlSchemeChange += OnChange;
+        }
+        protected virtual void OnDisable()
+        {
+            OnChangeKey -= OnChangeKeyCallBack;
+            QInputSystem.OnControlSchemeChange -= OnChange;
+            CleanUp();
+        }
+        void OnChangeKeyCallBack(QInputSetting keySetting, bool start)
+        {
+            if (!start)
+            {
+                if (keySetting.action == action)
+                {
+                    OnChange();
+                }
+            }
+        }
+        #region 改键逻辑
         public static Action<QInputSetting, bool> OnChangeKey;
         private InputActionRebindingExtensions.RebindingOperation ChangeOperation;
         void CleanUp()
@@ -19,7 +92,7 @@ namespace QTool.InputSystem
         }
         private void PerformInteractiveRebind(InputAction action)
         {
-            ChangeOperation?.Cancel(); 
+            ChangeOperation?.Cancel();
 
             ChangeOperation = action.PerformInteractiveRebinding(action.GetBindingIndex())
                 .OnCancel(
@@ -51,10 +124,7 @@ namespace QTool.InputSystem
             action.action.Disable();
             PerformInteractiveRebind(action);
         }
-        protected override  void OnDisable()
-        {
-            base.OnDisable();
-            CleanUp();
-        }
+
+        #endregion
     }
 }
